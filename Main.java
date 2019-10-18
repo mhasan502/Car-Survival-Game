5f35aa5008325564a6e7131cd07557014802bdc1
Mehedi Hasan <mhasan502@gmail.com>
2019-10-18 16:26:17 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,6 @@ import java.io.*;
 
 public class Main extends JFrame {
 	public Main() throws IOException {
-		JFrame frame = new JFrame("Car Survival");
-		GameDesign gameDesign = new GameDesign();
-		frame.add(gameDesign);
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(600,700);
-		frame.setVisible(true);
-		frame.setResizable(true);
+		new Watcher();
 	}
 }
