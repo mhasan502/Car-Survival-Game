bce63ac659ff164183beea39234a57411aa63e68
Mehedi Hasan <mhasan502@gmail.com>
2019-11-18 18:25:32 +0600

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,40 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
 
 public class Menu {
 	public static void main(String[] args) {
 
 		JFrame frame = new JFrame("Car Survival");
 		JButton button1 = new JButton("Play Game");
-		button1.setBounds(130,150,230, 70);
-		button1.setBackground(new Color(52, 168, 64));
-		button1.setFont(new Font("Monospaced", Font.BOLD, 21));
-		button1.setForeground(Color.white);
-
-
 		JButton button2 = new JButton("Exit");
-		button2.setBounds(130,260,230, 70);
-		button2.setBackground(new Color(52, 168, 64));
-		button2.setFont(new Font("Monospaced", Font.BOLD, 21));
-		button2.setForeground(Color.white);
+		setButtonProperty(button1,button2);
 
-		button1.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					new Main();
-				} catch (IOException ex) {}
-				frame.setVisible(false);
-			}
+		button1.addActionListener(e -> {
+			try {
+				new Watcher();
+			} catch (Exception ignored) {}
+			frame.dispose();
 		});
-		button2.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
+		button2.addActionListener(e -> System.exit(0));
 
 		frame.add(button1);
 		frame.add(button2);
@@ -44,5 +25,16 @@ public class Menu {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(true);
+	}
+	static void setButtonProperty(JButton button1,JButton button2){
+		button1.setBounds(130,150,230, 70);
+		button1.setBackground(new Color(52, 168, 64));
+		button1.setFont(new Font("Monospaced", Font.BOLD, 21));
+		button1.setForeground(Color.white);
+
+		button2.setBounds(130,260,230, 70);
+		button2.setBackground(new Color(52, 168, 64));
+		button2.setFont(new Font("Monospaced", Font.BOLD, 21));
+		button2.setForeground(Color.white);
 	}
 }
