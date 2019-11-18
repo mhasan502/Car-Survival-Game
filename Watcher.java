feed051dc75b98017f51593d1df1802c0238a505
Mehedi Hasan <mhasan502@gmail.com>
2019-11-18 18:20:04 +0600

    Update Watcher.java

--- Watcher.java
+++ Watcher.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class Watcher {
@@ -16,9 +14,9 @@ public class Watcher {
         frame.setVisible(true);
         frame.setResizable(true);
     }
-    public static void closing(int count) throws IOException{
+    public static void closing(int count) {
 
-        frame.setVisible(false);
+        frame.dispose();
         JFrame frame1 = new JFrame("Game Over");
         JButton scoreButton = new JButton("Score is: "+count);
         scoreButton.setBounds(0,100,500, 160);
@@ -27,34 +25,16 @@ public class Watcher {
         scoreButton.setForeground(Color.black);
 
         JButton button1 = new JButton("Play Again");
-        button1.setBounds(130,360,230, 70);
-        button1.setBackground(new Color(176, 0, 80));
-        button1.setFont(new Font("Monospaced", Font.BOLD, 21));
-        button1.setForeground(Color.white);
-
         JButton button2 = new JButton("Exit");
-        button2.setBounds(130,440,230, 70);
-        button2.setBackground(new Color(176, 0, 80));
-        button2.setFont(new Font("Monospaced", Font.BOLD, 21));
-        button2.setForeground(Color.white);
+        setButtonProperty(button1,button2);
 
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    new Main();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-                frame1.setVisible(false);
-            }
+        button1.addActionListener(e -> {
+            try {
+                new Main();
+            } catch (IOException ignored) {}
+            frame1.dispose();
         });
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        button2.addActionListener(e -> System.exit(0));
 
         frame1.add(button1);
         frame1.add(button2);
@@ -64,5 +44,16 @@ public class Watcher {
         frame1.setLayout(null);
         frame1.setVisible(true);
         frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    static void setButtonProperty(JButton button1,JButton button2){
+        button1.setBounds(130,360,230, 70);
+        button1.setBackground(new Color(176, 0, 80));
+        button1.setFont(new Font("Monospaced", Font.BOLD, 21));
+        button1.setForeground(Color.white);
+
+        button2.setBounds(130,440,230, 70);
+        button2.setBackground(new Color(176, 0, 80));
+        button2.setFont(new Font("Monospaced", Font.BOLD, 21));
+        button2.setForeground(Color.white);
     }
 }
